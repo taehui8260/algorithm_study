457220d6f69544c503849574a60aab2c8d27dee4
taehui8260 <taehui8260@gmail.com>
2024-01-16 17:02:27 +0900

    뉴스 클리어링 문제 진행중

---
+++ src/main/java/com/lth/algorithm/programmers/NewsClearing.java
@@ -0,0 +1,14 @@
+package com.lth.algorithm.programmers;
+
+public class NewsClearing {
+    public static void main(String[] args) {
+
+    }
+    static public int solution(String str1, String str2) {
+        int answer = 0;
+        return answer;
+    }
+    static boolean checkString(String str){
+        return str.matches(".*[\\s\\d[^a-zA-Z0-9]]+.*");
+    }
+}
