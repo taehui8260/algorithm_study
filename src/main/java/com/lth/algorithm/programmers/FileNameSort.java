1703711c4b4982200b4a4e05e351dac7954dc9d0
taehui8260 <taehui8260@gmail.com>
2024-01-30 19:26:20 +0900

    코드추가

--- src/main/java/com/lth/algorithm/programmers/FileNameSort.java
+++ src/main/java/com/lth/algorithm/programmers/FileNameSort.java
@@ -4,22 +4,24 @@ import java.util.*;
 
 public class FileNameSort {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new String[]{"aimG.12.2.JPG", "img12.png", "A10.png", "img02.png", "img1.png", "IMG01.GIF", "aimg.12.2.JPG"})));
+        System.out.println(Arrays.toString(solution(new String[]{"IMG0145.GIF", "aimG.12.2.JPG", "img12.png", "A10.png", "img02.png", "img1.png", "IMG01.GIF", "aimg.12.2.JPG", "a042ss", "a1234", "a2222", "aa2222"})));
     }
     //숫자를 기준으로 나오기 전 까지를 Head, 숫자를 Number, 뒤를 Tail 로 구분
     public static String[] solution(String[] files) {
-        Arrays.sort(files, (o1, o2) -> {
-            FileInfo file1 = new FileInfo(o1);
-            FileInfo file2 = new FileInfo(o2);
-            int headCompare = file1.head.compareTo(file2.head);
-            int numCompare = file1.num - file2.num;
+        FileInfo[] fileInfos = new FileInfo[files.length];
+        System.out.println(files.length);
 
+        for(int i=0; i< files.length; i++){
+            fileInfos[i] = new FileInfo(files[i]);
+            System.out.println();
+
+        }
+
+        Arrays.sort(fileInfos, (o1, o2) -> {
+            int headCompare = o1.head.compareTo(o2.head);
+            int numCompare = o1.num - o2.num;
             if(headCompare == 0){
-                if(numCompare == 0){
-                    return 0;
-                } else{
-                    return numCompare;
-                }
+                return numCompare;
 
             } else {
                 return headCompare;
@@ -32,18 +34,40 @@ public class FileNameSort {
         String head;
         int num;
 
-        int index;
+        int startNumIndex;
+        int startTailIndex;
+
         FileInfo(String fileFullName){
             this.fileFullName = fileFullName;
+            this.
+        }
+        String setHead(String fileFullName){
             for(int i=0; i<fileFullName.length(); i++){
-                if(this.index == 0 && Character.isDigit(fileFullName.charAt(i))){
-                    this.index = i;
-                    this.head =  fileFullName.substring(0, i).toUpperCase();
+                if(Character.isDigit(fileFullName.charAt(i))){
+                    this.startNumIndex = i;
+                    return fileFullName.substring(0, i).toUpperCase();
                 }
-                if(this.index !=0 && !Character.isDigit(fileFullName.charAt(i))){
-                    this.num = Integer.parseInt(fileFullName.substring(this.index, i));
-                    break;
+            }
+            return "";
+        }
+        int setNum(String fileFullName) {
+            int startTailIndex = -1;
+            for (int i = this.startNumIndex; i < fileFullName.length(); i++) {
+                if (!Character.isDigit(fileFullName.charAt(i))) {
+                    if (i - this.startNumIndex > 5) {
+                        startTailIndex = this.startNumIndex + 5;
+                        return Integer.parseInt(fileFullName.substring(this.startNumIndex, this.startTailIndex + 5));
+                    } else {
+                        startTailIndex = i;
+                        return Integer.parseInt(fileFullName.substring(this.startNumIndex, i));
+                    }
                 }
+            }
+            //Tail이 없을경우
+            if(fileFullName.substring(this.startNumIndex).length() > 5){
+                return Integer.parseInt(fileFullName.substring(this.startNumIndex, this.startNumIndex + 5));
+            } else{
+                return Integer.parseInt(fileFullName.substring(this.startNumIndex));
             }
         }
     }
