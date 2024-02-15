bcf97acacbc2e6b9909964d179d3938c01ab5335
taehui8260 <taehui8260@gmail.com>
2024-02-16 00:56:27 +0900

    UPDATE: 큰 수 정렬

--- src/main/java/com/lth/algorithm/programmers/Sort.java
+++ src/main/java/com/lth/algorithm/programmers/Sort.java
@@ -6,21 +6,41 @@ import java.util.Comparator;
 public class Sort {
     public static void main(String[] args) {
         Sort sort = new Sort();
-        System.out.println(sort.solution(new int[]{3, 30, 34, 5, 9, 307 ,0}));
+        System.out.println(sort.solution(new int[]{300,30030030, 12, 4, 647467454, 2222, 2, 0, 260}));
     }
+    // 길이가 같을 경우 그냥 비교한다.
+    // 길이가 다를경우 맨 가장 앞부터 하나씩 비교
+    // 끝까지 비교했는데 같은값이 나올경우 짧은 문자열의 길이만큼 긴 문자열을 자른 후 비교
+    // 위의 과정을 계속 반복
+
+    // 만약 문자열의 앞자리가 0이면 0 출력
     public String solution(int[] numbers) {
         StringBuilder answer = new StringBuilder();
         String[] answerArray = new String[numbers.length];
         for(int i=0; i<numbers.length;i++) {
             answerArray[i] = String.valueOf(numbers[i]);
         }
-        // 첫 번째 글자를 기준으로 내림차순으로 정렬하는 Comparator
         Comparator<String> customComparator = new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
-                if(s1.equals("0") || s2.equals("0"))
-                    return s2.compareTo(s1);
-                return s2.split("0")[0].compareTo(s1.split("0")[0]);
+                while(true) {
+                    if(s1.length() ==  s2.length())
+                        return s2.compareTo(s1);
+                    if(s1.length() <  s2.length()) {
+                        for (int i = 0; i < s1.length(); i++) {
+                            if (s1.charAt(i) != s2.charAt(i))
+                                return s2.substring(i, i + 1).compareTo(s1.substring(i, i + 1));
+                        }
+                        s2 = s2.substring(s1.length());
+                    }
+                    if(s1.length() >  s2.length()) {
+                        for (int i = 0; i < s2.length(); i++) {
+                            if (s1.charAt(i) != s2.charAt(i))
+                                return s2.substring(i, i + 1).compareTo(s1.substring(i, i + 1));
+                        }
+                        s1 = s1.substring(s2.length());
+                    }
+                }
             }
         };
         // Comparator를 사용하여 배열 정렬
@@ -28,6 +48,6 @@ public class Sort {
         for (String item : answerArray){
             answer.append(item);
         }
-        return String.valueOf(answer);
+        return answer.substring(0,1).equals("0")?"0":String.valueOf(answer);
     }
 }
