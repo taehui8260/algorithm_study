da90c1382c87126ff5b1b4b26c5a419075c694ae
taehui8260 <taehui8260@gmail.com>
2024-03-07 19:13:32 +0900

    UPDATE: 알고리즘 문제풀이
    방금 그곡 진행중

---
+++ src/main/java/com/lth/algorithm/programmers/level2/ThatSongJustNow.java
@@ -0,0 +1,28 @@
+package com.lth.algorithm.programmers.level2;
+
+public class ThatSongJustNow {
+    public static void main(String[] args) {
+        ThatSongJustNow taThatSongJustNow = new ThatSongJustNow();
+    }
+
+    /**
+     * 방금 그곡
+     *
+     * 4개씩 끊어서 곡에 대한 정보가 들어있다
+     * 곡 시작시간 / 곡 종료시간 / 곡 이름 / 곡 내용
+     * 곡은 시작시간부터 종료시간까지 반복해서 계속 나온다
+     * 문자열 m은 기억한 곡의 정보가 들어있다.
+     * 모든 곡 정보를 곡에 재생된 음악 정보를 추가해 곡 정보 Class 에 추가한다.
+     * 기억한 곡의 정보가 재생된 곡 정보에 포함된다면 해당 곡에 대한 정보 List에 넣고 가장 긴 재생시간을 저장한다.
+     * 만약 값이 어러개라면 재생 시간이 가장 긴 음악을 반환한다.
+     * 이래도 값이 여러개라면 0번째 곡을 보여준다.
+     *
+     * @param m
+     * @param musicinfos
+     * @return
+     */
+    public String solution(String m, String[] musicinfos) {
+        String answer = "";
+        return answer;
+    }
+}
