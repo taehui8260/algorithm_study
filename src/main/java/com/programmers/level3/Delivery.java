0cc8f9b6529e8a394d8dcbe17952984200c9a2f0
taehui8260 <taehui8260@gmail.com>
2024-04-09 22:26:41 +0900

    UPDATE: 프로그래머스 문제풀이
    Level3 배달 진행중

---
+++ src/main/java/com/programmers/level3/Delivery.java
@@ -0,0 +1,18 @@
+package com.programmers.level3;
+
+/**
+ * 배달
+ */
+public class Delivery {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
