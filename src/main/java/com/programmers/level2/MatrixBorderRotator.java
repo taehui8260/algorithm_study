0da732ca569e8e01846e2c4f2b78bf7dca731e72
taehui8260 <taehui8260@gmail.com>
2024-04-29 23:52:52 +0900

    UPDATE: 프로그래머스 문제풀이
    level2 행열 테두리 회전하기

---
+++ src/main/java/com/programmers/level2/MatrixBorderRotator.java
@@ -0,0 +1,11 @@
+package com.programmers.level2;
+
+/**
+ * 행열 테두리 회전하기
+ */
+public class MatrixBorderRotator {
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+}
