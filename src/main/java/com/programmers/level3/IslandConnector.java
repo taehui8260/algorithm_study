838264ef2ac944f86c4daf1f429219eb423b708f
taehui8260 <teahui8260>
2024-05-16 13:14:54 +0900

    UPDATE: 프로그래머스 알고리즘
    level3 섬 연결하기
    
    파인드 유니온 알고리즘 이용

--- src/main/java/com/programmers/level3/IslandConnector.java
+++ src/main/java/com/programmers/level3/IslandConnector.java
@@ -9,25 +9,38 @@ import java.util.*;
 public class IslandConnector {
     public static void main(String[] args) {
        IslandConnector islandConnector = new IslandConnector();
-        System.out.println(islandConnector.solution(4,	new int[][]{{0, 1, 10}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}}));
+        System.out.println(islandConnector.solution(4,	new int[][]{{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}}));
     }
     public int solution(int n, int[][] costs) {
         int answer = 0;
         Arrays.sort(costs, (a, b) ->{
            return a[2] - b[2];
         });
+        int [] parents = new int[n];
+        for(int i=0; i<n; i++){
+            parents[i] = i;
+        }
         for(int[] cost : costs){
             System.out.println(Arrays.toString(cost));
-        }
-        boolean [] checkVisit = new boolean[n];
-
-        for(int[] cost : costs){
-            if(!checkVisit[cost[0]] || !checkVisit[cost[1]]){
+            if(find(parents, cost[0]) != find(parents, cost[1])){
                 answer += cost[2];
-                checkVisit[cost[0]] = true;
-                checkVisit[cost[1]] = true;
+                union(parents, cost[0], cost[1]);
             }
+            System.out.println(Arrays.toString(parents));
         }
         return answer;
+    }
+    private int find(int[] parents, int n){
+        if(parents[n] == n){
+            return n;
+        } else{
+            return parents[n] = find(parents, parents[n]);
+        }
+    }
+    private void union(int[] parents, int x, int y){
+        int xParents = find(parents, x);
+        int yParents = find(parents, y);
+        if(xParents != yParents)
+            parents[xParents] = yParents;
     }
 }
