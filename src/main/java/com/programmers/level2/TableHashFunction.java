b953ff367ea32e18b4a555992cc72f537478c38b
taehui8260 <taehui8260@gmail.com>
2024-05-29 18:38:06 +0900

    UPDATE: 프로그래머스 알고리즘
    level2 테이블 해시 함수 진행중

--- src/main/java/com/programmers/level2/TableHashFunction.java
+++ src/main/java/com/programmers/level2/TableHashFunction.java
@@ -1,7 +1,30 @@
 package com.programmers.level2;
 
+import java.util.Arrays;
+
 /**
  * 테이블 해시 함수
+ *
+ *
  */
 public class TableHashFunction {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        Arrays.sort(data, (o1, o2) ->
+            o1[col-1] - o2[col-1]
+        );
+        int[] S_i = new int[row_end - row_begin + 1];
+        for(int i=0; i<S_i.length; i++){
+            for(int item: data[row_begin + i]){
+                S_i[i] += item % (row_begin + i);
+            }
+        }
+        for(int i=0; i<S_i.length; i++){
+            answer ^= S_i
+        }
+        return answer;
+    }
 }
