b737de434f616e67cb4860058e5207a1413b1811
taehui8260 <taehui8260@gmail.com>
2024-06-03 23:49:01 +0900

    UPDATE: 프로그래머스 문제풀이
    level2 가장 큰 정사각형 찾기

---
+++ src/main/java/com/programmers/level2/LargestSquareFinder.java
@@ -0,0 +1,23 @@
+package com.programmers.level2;
+
+/**
+ * 가장 큰 정사각형 찾기
+ */
+public class LargestSquareFinder {
+    public int solution(int [][]board) {
+        boolean [][] check = new boolean[board.length][board[0].length];
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+
+            }
+        }
+    }
+    private int findSquare(int row, int col, int squareSize, int [][]board, boolean [][] check){
+        for(int i=0; i<squareSize; i++){
+            if(board[row+squareSize][col]!=1)
+        }
+        for(int i=0; i<squareSize; i++){
+            if(rowDirection[i] + row < board.length && colDirection[i] <)
+        }
+    }
+}
