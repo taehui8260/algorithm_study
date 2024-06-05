1510e0c6bf757e269510c0b16a02ae2374eecfa2
taehui8260 <taehui8260@gmail.com>
2024-06-05 23:37:28 +0900

    UPDATE: 프로그래머스 문제풀이
    level3 디스크 컨트롤러

---
+++ src/main/java/com/programmers/level3/DiskController.java
@@ -0,0 +1,12 @@
+package com.programmers.level3;
+
+/**
+ * 디스크 컨트롤러
+ *
+ * 해당 시점에 가장 작은 소요  시간이 걸리는 내용을 처리
+ */
+public class DiskController {
+    public double solution(int[][] jobs) {
+
+    }
+}
