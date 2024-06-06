5266cc29a382afcd0b0f0a7a0af0355084b24e7d
taehui8260 <taehui8260@gmail.com>
2024-06-06 14:32:07 +0900

    UPDATE: 프로그래머스 문제풀이
    level3 디스크 컨트롤러

--- src/main/java/com/programmers/level3/DiskController.java
+++ src/main/java/com/programmers/level3/DiskController.java
@@ -14,32 +14,32 @@ import java.util.PriorityQueue;
  */
 public class DiskController {
     public double solution(int[][] jobs) {
+        // 작업을 시작 시간에 따라 정렬
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+
         int totalTime = 0;
-        int currentTime = jobs[0][0];
+        int currentTime = 0;
+        int index = 0;
+        int count = 0;
         PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
 
-        for(int i=0; i< jobs.length;){
-            if(jobs[i][0] <= currentTime){
-                priorityQueue.offer(jobs[i]);
-                i++;
-                continue;
+        while (count < jobs.length) {
+            // 현재 시간보다 시작 시간이 빠른 모든 작업을 큐에 넣음
+            while (index < jobs.length && jobs[index][0] <= currentTime) {
+                priorityQueue.offer(jobs[index]);
+                index++;
             }
-            if(!priorityQueue.isEmpty()){
-                int [] job = priorityQueue.poll();
-                totalTime += currentTime - job[0] + job[1];
-                currentTime += job[1];
-            } else{
-                priorityQueue.offer(jobs[i]);
-                currentTime = jobs[i][0];
-                i++;
-            }
-        }
 
-        while(!priorityQueue.isEmpty()){
-            int [] job = priorityQueue.poll();
-            totalTime += currentTime - job[0] + job[1];
-            currentTime += job[1];
+            if (!priorityQueue.isEmpty()) {
+                // 우선순위 큐에서 소요 시간이 가장 짧은 작업을 꺼냄
+                int[] job = priorityQueue.poll();
+                currentTime = Math.max(currentTime, job[0]) + job[1];
+                totalTime += currentTime - job[0];
+                count++;
+            } else if (index < jobs.length) {
+                // 큐가 비어있다면 현재 시간을 다음 작업의 시작 시간으로 업데이트
+                currentTime = jobs[index][0];
+            }
         }
         return (double) totalTime / jobs.length;
     }
