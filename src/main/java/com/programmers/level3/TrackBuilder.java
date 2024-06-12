1f6ff18baab00cbb1d59989bbce53e14535bd9b0
taehui8260 <taehui8260@gmail.com>
2024-06-12 22:57:55 +0900

    UPDATE: 프로그래머스 문제풀이
    level3 경주로건설

---
+++ src/main/java/com/programmers/level3/TrackBuilder.java
@@ -0,0 +1,12 @@
+package com.programmers.level3;
+
+/**
+ * 경주로 건설
+ *
+ */
+public class TrackBuilder {
+    public int solution(int[][] board) {
+        int answer = 0;
+        return answer;
+    }
+}
