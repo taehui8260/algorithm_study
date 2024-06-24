f87f6da6231c83d7ce1837fb84653a01397f89a8
taehui8260 <teahui8260>
2024-06-24 18:32:07 +0900

    UPDATE: 프로그래머스 알고리즘
    level3  연속 펄스 부분 수열의 합

---
+++ src/main/java/com/programmers/level3/SubsequenceSum.java
@@ -0,0 +1,12 @@
+package com.programmers.level3;
+
+/**
+ * 연속 펄스 부분 수열의 합
+ *
+ */
+public class SubsequenceSum {
+    public long solution(int[] sequence) {
+        long answer = 0;
+        return answer;
+    }
+}
