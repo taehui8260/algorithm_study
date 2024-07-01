0d4923936d73dbd79274395457f09379b04c76f6
taehui8260 <taehui8260@gmail.com>
2024-07-01 23:27:36 +0900

    UPDATE: 프로그래머스 문제풀이
    level2 문자열 압축

--- src/main/java/com/programmers/level2/StringCompression.java
+++ src/main/java/com/programmers/level2/StringCompression.java
@@ -1,8 +1,36 @@
 package com.programmers.level2;
 
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 문자열 압축
+ *
+ * 약수로 짜른다.
+ */
 public class StringCompression{
     public int solution(String s) {
         int answer = 0;
+        String resultString = "";
+        List<Integer> divisors = findDivisors(s.length());
+        for(Integer divisor: divisors){
+            int index = 0;
+            for(int i=0; i<s.length()-divisor;){
+                if(s.substring(i, i+divisor).equals(s.substring(i+divisor, i+divisor+divisor))){
+
+                }
+                i += divisor;
+            }
+        }
         return answer;
+    }
+    private List<Integer> findDivisors(int n){
+        List<Integer> result = new LinkedList<>();
+        for(int i=2; i<n; i++){
+            if(n%2 ==0){
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
