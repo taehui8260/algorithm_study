04de60a247b0c6f24dabb68c65ab306528e11121
taehui8260 <taehui8260@gmail.com>
2024-07-04 23:47:56 +0900

    UPDATE: 프로그래머스 문제풀이
    level2 문자열 압축

--- src/main/java/com/programmers/level2/StringCompression.java
+++ src/main/java/com/programmers/level2/StringCompression.java
@@ -12,55 +12,42 @@ public class StringCompression {
 
     public static void main(String[] args) {
         StringCompression stringCompression = new StringCompression();
-        System.out.println(stringCompression.solution("abcabcdede"));
+        System.out.println(stringCompression.solution("aabbaccc"));
     }
 
     public int solution(String s) {
         int answer = Integer.MAX_VALUE;
-        List<Integer> divisors = findDivisors(s.length());
-        System.out.println(divisors);
-        for (Integer divisor : divisors) {
-            String resultString = "";
+        for (int divisor = 1; divisor <= s.length()/2; divisor++) {
+            StringBuilder resultString = new StringBuilder();
             int index = 1;
             String standardString = s.substring(0, divisor);
-            for (int i = divisor; i <= s.length() - divisor; ) {
+            int i = divisor;
+            while (true) {
+                if(i + divisor > s.length()){
+                    if (index != 1) {
+                        resultString.append(Integer.toString(index)).append(standardString).append(s.substring(i));
+                    } else {
+                        resultString.append(standardString).append(s.substring(i));
+                    }
+                    break;
+                }
+
                 String substring = s.substring(i, i + divisor);
                 if (standardString.equals(substring)) {
                     index++;
-                    if (i == s.length() - divisor) {
-                        if (index != 1) {
-                            resultString = resultString + Integer.toString(index) + standardString;
-                        } else {
-                            resultString = resultString + standardString;
-                        }
-                        break;
-                    }
                 } else {
                     if (index != 1) {
-                        resultString = resultString + Integer.toString(index) + standardString;
+                        resultString.append(Integer.toString(index)).append(standardString);
                     } else {
-                        resultString = resultString + standardString;
+                        resultString.append(standardString);
                     }
                     standardString = substring;
                     index = 1;
                 }
                 i += divisor;
             }
-            System.out.println("### standardString: " + standardString);
-            System.out.println("### resultString: " + resultString);
             answer = Math.min(answer, resultString.length());
         }
-
         return answer == Integer.MAX_VALUE ? s.length() : answer;
-    }
-
-    private List<Integer> findDivisors(int n) {
-        List<Integer> result = new LinkedList<>();
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                result.add(i);
-            }
-        }
-        return result;
     }
 }
