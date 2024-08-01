44fa6dc377c14e002597f3a3a91d90d8ab9755b7
taehui8260 <taehui8260@gmail.com>
2024-08-02 00:27:48 +0900

    UPDATE: 프로그래머스 문제풀이
    level3 다단계 칫솔 판매

--- src/main/java/com/programmers/level3/MultilevelToothbrushSales.java
+++ src/main/java/com/programmers/level3/MultilevelToothbrushSales.java
@@ -1,7 +1,6 @@
 package com.programmers.level3;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * 다단계 칫솔 판매
@@ -13,19 +12,15 @@ public class MultilevelToothbrushSales {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int[] answer = new int[enroll.length];
         Map<String, String> sellerChain = new HashMap<>();
-        Map<String, List<Integer>> sellerAmount = new HashMap<>();
         Map<String, Integer> totalSellerAmount = new LinkedHashMap<>();
 
         for(int i=0; i<enroll.length; i++){
             sellerChain.put(enroll[i], referral[i]);
             totalSellerAmount.put(enroll[i], 0);
         }
+
         for(int i=0; i<seller.length; i++){
-            sellerAmount.putIfAbsent(seller[i], new ArrayList<>());
-            sellerAmount.get(seller[i]).add(amount[i] * 100);
-        }
-        for(String sellerName: sellerAmount.keySet()){
-            dfs(sellerChain, totalSellerAmount, sellerName, sellerAmount.get(sellerName));
+            dfs(sellerChain, totalSellerAmount, seller[i], amount[i] * 100);
         }
 
         int index = 0;
@@ -34,15 +29,15 @@ public class MultilevelToothbrushSales {
         }
         return answer;
     }
-    void dfs(Map<String, String> sellerChain, Map<String, Integer> totalSellerAmount, String seller, List<Integer> amountList){
-        List<Integer> enrollAmount = amountList.stream().map(n -> n/10).collect(Collectors.toList());
-        int sellerAmount = 0;
-        for(Integer amount: amountList){
-            sellerAmount += (amount/10) * 9 + (amount%10);
-        }
-        totalSellerAmount.merge(seller, sellerAmount, Integer::sum);
-        if(!sellerChain.get(seller).equals("-")){
-            dfs(sellerChain, totalSellerAmount, sellerChain.get(seller), enrollAmount);
+    void dfs(Map<String, String> sellerChain, Map<String, Integer> totalSellerAmount, String seller, int amount){
+        int enrollAmount;
+        int sellerAmount;
+        while(!seller.equals("-") && amount > 0) {
+            enrollAmount = amount/10;
+            sellerAmount = amount - enrollAmount;
+            totalSellerAmount.merge(seller, sellerAmount, Integer::sum);
+            seller = sellerChain.get(seller);
+            amount = enrollAmount;
         }
     }
 }
