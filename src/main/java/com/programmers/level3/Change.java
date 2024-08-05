1bfa661691afeb80582634069e9d8438260cd207
DESKTOP-4IDT16J\lth11 <lth1178@gmail.com>
2024-08-05 22:43:39 +0900

    UPDATE: 프로그래머스
    level3 거스름돈

--- src/main/java/com/programmers/level3/Change.java
+++ src/main/java/com/programmers/level3/Change.java
@@ -1,15 +1,26 @@
 package com.programmers.level3;
 
+import java.util.Arrays;
+
 /**
  * 거스름돈
  *
- * moneyf를 오름차순 정렬
+ * money를 오름차순 정렬
  *
- * 뒤에서 부터 만드는 수를 찾는다.
+ * dp[0] 부터  dp[n] 까지 0 ~ n원을 거스름 돈으로 줄 수 있는 경우를 구한다.
  */
 public class Change {
     public int solution(int n, int[] money) {
-        int answer = 0;
-        return answer;
+        int [] changeAry = new int [n+1];
+        Arrays.sort(money);
+        int MOD = 1000000007;
+        changeAry[0] = 1;
+
+        for(int coin: money){
+            for(int i=coin; i<n+1; i++){
+                changeAry[i] = (changeAry[i] + changeAry[i-coin])/MOD;
+            }
+        }
+        return changeAry[n];
     }
 }
