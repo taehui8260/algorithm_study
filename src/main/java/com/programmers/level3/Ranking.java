99d1c48667b2ad9220ff8b9e60be8393ae817a8e
taehui8260 <taehui8260@gmail.com>
2024-08-13 00:24:26 +0900

    UPDATE: 프로그래머스
    level3 순위

---
+++ src/main/java/com/programmers/level3/Ranking.java
@@ -0,0 +1,11 @@
+package com.programmers.level3;
+
+/**
+ * 순위
+ */
+public class Ranking {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        return answer;
+    }
+}
