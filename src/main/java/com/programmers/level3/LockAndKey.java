c91bfff233224c943fd626ecfa07efeab5493072
taehui8260 <taehui8260@gmail.com>
2024-08-28 01:28:30 +0900

    UPDATE: 프로그래머스
    level3 자물쇠와 열쇠

---
+++ src/main/java/com/programmers/level3/LockAndKey.java
@@ -0,0 +1,11 @@
+package com.programmers.level3;
+
+/**
+ * 자물쇠와 열쇠
+ */
+public class LockAndKey {
+    public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+        return answer;
+    }
+}
