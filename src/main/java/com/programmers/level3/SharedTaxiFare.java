dc1a6528ac666dde22cb0f0758513fa6c70864a8
taehui8260 <taehui8260@gmail.com>
2024-09-04 00:45:03 +0900

    UPDATE: 프로그래머스
    level3 택시 합승 요금

---
+++ src/main/java/com/programmers/level3/SharedTaxiFare.java
@@ -0,0 +1,17 @@
+package com.programmers.level3;
+import java.util.*;
+/**
+ * 택시 합승 요금
+ */
+public class SharedTaxiFare {
+    public static void main(String[] args) {
+
+        PriorityQueue<String> priorityQue = new PriorityQueue();
+        Queue<String> queue = new LinkedList<>();
+
+        Integer integer = Integer.valueOf("#");
+        String string = "test";
+
+    }
+
+}
