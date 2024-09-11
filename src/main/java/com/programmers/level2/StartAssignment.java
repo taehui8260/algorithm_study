fa32421b0521b3d340990f780542149e0977f4da
taehui8260 <taehui8260@gmail.com>
2024-09-12 00:12:10 +0900

    UPDATE: 프로그래머스
    level2 과제 시작

---
+++ src/main/java/com/programmers/level2/StartAssignment.java
@@ -0,0 +1,12 @@
+package com.programmers.level2;
+
+/**
+ * 과제 시작
+ */
+public class StartAssignment {
+
+    public String[] solution(String[][] plans) {
+        String[] answer = {};
+        return answer;
+    }
+}
